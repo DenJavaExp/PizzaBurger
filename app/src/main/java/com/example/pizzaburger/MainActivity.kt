7ae96767e182a35acf34ad9afd2231401ae30576
marmy <marmyshev.denis@mail.ru>
2024-12-12 14:58:07 +0200

    RA-04

--- app/src/main/java/com/example/pizzaburger/MainActivity.kt
+++ app/src/main/java/com/example/pizzaburger/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.pizzaburger
 import CategoriesListFragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import com.example.pizzaburger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +15,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            val fragmentContainerView = CategoriesListFragment()
-            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-            transaction.add(R.id.mainContainer, fragmentContainerView)
-            transaction.commit()
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(R.id.mainContainer, CategoriesListFragment::class.java, null)
+            }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
