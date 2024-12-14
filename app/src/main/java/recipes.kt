baaff14f3f09f2ab98dc9414d6670a3f3c89d730
marmy <marmyshev.denis@mail.ru>
2024-12-14 17:16:26 +0200

    RA-06

--- app/src/main/java/recipes.kt
+++ app/src/main/java/recipes.kt
@@ -8,31 +8,31 @@ object STUB {
             imageUrl = "burger.png"
         ),
         Category(
-            id = 2,
+            id = 1,
             title = "Десерты",
             description = "Самые вкусные рецепты десертов специально для вас",
             imageUrl = "dessert.png"
         ),
         Category(
-            id = 3,
+            id = 2,
             title = "Пицца",
             description = "Пицца на любой вкус и цвет. Лучшая подборка для тебя",
             imageUrl = "pizza.png"
         ),
         Category(
-            id = 4,
+            id = 3,
             title = "Рыба",
             description = "Печеная, жареная, сушеная, любая рыба на твой вкус",
             imageUrl = "fish.png"
         ),
         Category(
-            id = 5,
+            id = 4,
             title = "Супы",
             description = "От классики до экзотики: мир в одной тарелке",
             imageUrl = "soup.png"
         ),
         Category(
-            id = 6,
+            id = 5,
             title = "Салаты",
             description = "Хрустящий калейдоскоп под соусом вдохновения",
             imageUrl = "salad.png"
