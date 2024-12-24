0e33d175b7e4e56e824cfe765e24181a310b2d57
marmy <marmyshev.denis@mail.ru>
2024-12-24 17:39:32 +0200

    RA-09

--- app/src/main/java/CategoriesListFragment.kt
+++ app/src/main/java/CategoriesListFragment.kt
@@ -3,10 +3,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.example.pizzaburger.R
 import com.example.pizzaburger.databinding.FragmentCategoriesListBinding
+import models.Category
 
 
-class CategoriesListFragment: Fragment() {
+class CategoriesListFragment : Fragment() {
 
     private var _binding: FragmentCategoriesListBinding? = null
     private val binding
@@ -35,5 +39,21 @@ class CategoriesListFragment: Fragment() {
     private fun initRecycler() {
         val categoryAdapter = CategoriesListAdapter(STUB.getCategories())
         binding.rvCategories.adapter = categoryAdapter
+
+        categoryAdapter.setOnItemClickListener(object : CategoriesListAdapter.OnItemClickListener {
+            override fun onItemClick(category: Category) {
+
+                openRecipesByCategoryId()
+
+            }
+        })
+    }
+
+    private fun openRecipesByCategoryId() {
+        requireActivity().supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            addToBackStack(null)
+            replace<RecipesListFragment>(R.id.mainContainer)
+        }
     }
 }
